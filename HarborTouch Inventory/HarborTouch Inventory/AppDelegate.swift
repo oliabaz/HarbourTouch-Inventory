f05786cac46c84e556ed59d69a69fca466635f41
Bazyleva, Olga <o.bazyleva@itransition.com>
2016-07-12 12:08:37 +0300

    - menuScreen

--- HarborTouch Inventory/HarborTouch Inventory/AppDelegate.swift
+++ HarborTouch Inventory/HarborTouch Inventory/AppDelegate.swift
@@ -15,7 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let navBarImage:UIImage! = UIImage(named: "navBar.png")
+        UINavigationBar.appearance().setBackgroundImage(navBarImage, forBarMetrics: .Default)
+
         return true
     }
 
